fe07a833587102116c3412bc7034036dd507def8
Evander Smid Gidion <smidgidionevander@gmail.com>
2026-04-23 15:11:12 +0700

    modified:   assets/js/config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 export const APP_CONFIG = Object.freeze({
   APP_NAME: "POS Kantin",
   APP_VERSION: "0.1.0",
-  USE_MOCK_API: false,
-  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxBfKHfBZepTvTzlHePgvRVGg1TCeMielXlfZmT8kYW8zmiQOIayUkgwaRBEUsSEKsgaA/exec",
+  USE_MOCK_API: true,
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxYmz2sRretalaCfYWv0FsMNIZE84zCTDTQTA1xbXC8lslimi8MXNvNzsKX75C61qXPKg/exec",
   REQUEST_TIMEOUT_MS: 15000,
   STORAGE_KEYS: {
     session: "pos-kantin.session",
